9b42cd27483b6751d9ff9c1464c7322375c354c2
MCqueen9500 <n.bankar1977@gmail.com>
2026-07-31 00:16:22 +0530

    Time: 11 ms (90.38%), Space: 46.1 MB (98.82%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,17 +1,70 @@
 class Solution {
+
     public String removeDuplicates(String s) {
-        StringBuilder stack = new StringBuilder();
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        StringBuilder str = new StringBuilder();
 
-            if (stack.length() > 0 && stack.charAt(stack.length() - 1) == ch) {
-                stack.deleteCharAt(stack.length() - 1);
-            } else {
-                stack.append(ch);
-            }
+        if(s.length() == 0){
+
+            return "";
+
         }
 
-        return stack.toString();
+        str.append(s.charAt(0));
+
+        char ch = s.charAt(0);
+
+        int j = 1;
+
+        for(int i = 1;i<s.length();i++){
+
+            if(ch == s.charAt(i)){
+
+                str.setLength(j - 1);
+
+                j-=1;
+
+                if(j == 0 ){
+                    if(i+1 < s.length()){
+                        ch = s.charAt(i+1);
+                        str.append(ch);
+                        j++;
+                    i++;
+                    }
+                    else 
+                    {
+                        ch = '.';
+                    }
+                }
+
+                else{
+
+                    ch = str.charAt(j-1);
+
+                }
+
+                
+
+            }
+
+            else{
+
+                ch = s.charAt(i);
+
+                str.append(ch);
+
+                j++;
+
+            }
+
+
+
+        }
+
+        String ans = str.toString();
+
+        return ans;
+
     }
+
 }
