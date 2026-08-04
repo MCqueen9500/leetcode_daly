3cfee078ad40f2cc04cd5202ebe7961e0fb6b301
MCqueen9500 <n.bankar1977@gmail.com>
2026-08-04 19:02:11 +0530

    Time: 1 ms (99.96%), Space: 71 MB (94.04%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,16 +1,21 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-        int ans = 1;
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i]==ans){
-                ans++;
+       for(int i = 0;i<nums.length;i++){
+        if(1 <= nums[i] && nums[i] <=nums.length){
+            if(nums[nums[i] - 1] == nums[i]){
                 continue;
             }
-            else if(nums[i] > ans){
-                return ans;
-            }
+            int temp = nums[nums[i] - 1];
+            nums[nums[i] - 1] = nums[i];
+            nums[i] = temp;
+            i--;
         }
-        return ans;
+       }
+       for(int i = 0;i<nums.length;i++){
+        if(nums[i] != i+1){
+            return i+1;
+        }
+       }
+       return nums.length + 1;
     }
 }
