c218c4f817a6d27967dd91d54782e80e4df8220e
MCqueen9500 <n.bankar1977@gmail.com>
2026-08-24 18:42:57 +0530

    Time: 1 ms (100%), Space: 118.6 MB (78.88%) - LeetHub

--- 3979-maximum-valid-pair-sum/3979-maximum-valid-pair-sum.java
+++ 3979-maximum-valid-pair-sum/3979-maximum-valid-pair-sum.java
@@ -1,24 +1,18 @@
 class Solution {
-    int max(int nums[],int i){
-        int max = Integer.MIN_VALUE;
-        for(int k = i;k<nums.length;k++){
-            if(nums[k]>max){
-                max = nums[k];
-            }
-        }
-        return max;
-    }
+   
     public int maxValidPairSum(int[] nums, int k) {
         int MaxSum = Integer.MIN_VALUE;
-        int Max = max(nums,k);
-        for(int i = 0;i<nums.length - k;i++){
-           int sum = Max + nums[i];
+        int Max = nums[0];
+        int i = 0;
+        for(int j = k;j<nums.length ;j++){
+           if(Max < nums[i]){
+            Max = nums[i];
+           }
+           int sum = Max + nums[j];
            if(MaxSum < sum){
             MaxSum = sum;
            }
-           if(nums[i+k]==Max){
-            Max = max(nums,i+1+k);
-           }
+           i++;
         }
         return MaxSum;
     }
